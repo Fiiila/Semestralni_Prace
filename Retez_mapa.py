e7cece09f81c5a48e075f2ac6a3182b2b0a032a7
Filip_Jasek <filda.jasek@gmail.com>
2021-12-13 17:10:51 +0100

    update metody retezove mapy

--- Retez_mapa.py
+++ Retez_mapa.py
@@ -32,8 +32,8 @@ def vykresliRetezMapu(retezovaMapa, X, Y):
         bod1, bod2 = retezovaMapa[i][0]
         x1, y1 = X[bod1], Y[bod1]
         x2, y2 = X[bod2], Y[bod2]
-        plt.plot([x1, x2], [y1, y2], 'b')
-        plt.plot([x1, x2], [y1, y2], 'ro')
+        #plt.plot([x1, x2], [y1, y2], 'b')
+        plt.plot([x1, x2], [y1, y2], '-o', color='red')
     return
 
 if __name__ == "__main__":
